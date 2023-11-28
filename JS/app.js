baa47b050716abbdc165d3863b5654898cb89017
sarfrajabbasi <sarfrajk96811@gmail.com>
2023-11-28 14:00:25 +0530

    multiply app

--- JS/app.js
+++ JS/app.js
@@ -1,18 +1,28 @@
-function getElement(selector){
-    const element = document.querySelector(selector);
+function getElement(selector) {
+  const element = document.querySelector(selector);
 
-    if(element){
-        return element
-    }else{
-        throw Error('please check the selector')
-    }
+  if (element) {
+    return element;
+  } else {
+    throw Error("please check the selector");
+  }
 }
-const  randomNumber = (n)=>Math.floor(Math.random()*n)+ 1;
+const randomNumber = (n) => Math.floor(Math.random() * n) + 1;
 
 const NUM_1 = randomNumber(100);
-const NUM_2 = randomNumber(100)
+const NUM_2 = randomNumber(100);
 const ANSWER = NUM_1 * NUM_2;
-const QUESTION_EL = getElement("#question")
-QUESTION_EL.textContent = `what is ${NUM_1} multiply by ${NUM_2}`
 
-console.log(ANSWER);
+// element
+const QUESTION_EL = getElement("#question");
+const FORM_EL = getElement(".form");
+const INPUT_EL = getElement("#input");
+
+// use them
+QUESTION_EL.textContent = `what is ${NUM_1} multiply by ${NUM_2}`;
+
+// add event
+
+FORM_EL.addEventListener('submit',()=>{
+
+})
